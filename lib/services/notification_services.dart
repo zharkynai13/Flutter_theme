b3c0e5b320479488d40d8a5a75412ab601f813e6
Zharkynai Shabdanbekova <zharkynai.shabdanbekova.kk@gmail.com>
2023-05-31 20:40:14 +0600

    add noti-on

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,29 +1,70 @@
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+class NotifyHelper {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin(); 
+    initializeNotification() async {
+    //tz.initializeTimeZones();
 
-// FlutterLocalNotificationsPlugin
-//   flutterLocalNotificationsPlugin =
-//       FlutterLocalNotificationsPlugin(); 
-//     //initializeNotification() async {
-//     //tz.initializeTimeZones();
-//    // this is for latest iOS settings
-//     final DarwinInitializationSettings initializationSettingsIOS =
-//     DarwinInitializationSettings(
-//         requestSoundPermission: false,
-//         requestBadgePermission: false,
-//         requestAlertPermission: false,
-//         onDidReceiveLocalNotification: onDidReceiveLocalNotification
-//     );
+    final DarwinInitializationSettings initializationSettingsIOS =
+    DarwinInitializationSettings(
+        requestSoundPermission: false,
+        requestBadgePermission: false,
+        requestAlertPermission: false,
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification
+    );
 
-//  final Android InitializationSettings initializationSettingsAndroid =
-//      Android InitializationSettings("appicon);
+ final AndroidInitializationSettings initializationSettingsAndroid =
+     AndroidInitializationSettings("appicon");
 
-//     final InitializationSettings initializationSettings =
-//         InitializationSettings(
-//        iOS: initializationSettingsIOS,
-//        android:initializationSettingsAndroid,
-//     );
-//     await flutterLocalNotificationsPlugin.initialize(
-//         initializationSettings,
-//         onSelectNotification: selectNotification);
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+       iOS: initializationSettingsIOS,
+       android:initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(
+        initializationSettings,
+        // onDidReceiveNotificationResponse: selectNotification
+    );
+}
+Future selectNotification(String payload) async {
+    if (payload != null) {
+      print('notification payload: $payload');
+    } else {
+      print("Notification Done");
+    }
+     Get.to(()=>Container(color: Colors.white,));
+  }
+  Future onDidReceiveLocalNotification(
+      int id, String? title, String? body, String? payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    // showDialog(
+    //   //context: context,
+    //   builder: (BuildContext context) => CupertinoAlertDialog(
+    //     title: Text(title),
+    //     content: Text(body),
+    //     actions: [
+    //       CupertinoDialogAction(
+    //         isDefaultAction: true,
+    //         child: Text('Ok'),
+    //         onPressed: () async {
+    //           Navigator.of(context, rootNavigator: true).pop();
+    //           await Navigator.push(
+    //             context,
+    //             MaterialPageRoute(
+    //               builder: (context) => SecondScreen(payload),
+    //             ),
+    //           );
+    //         },
+    //       )
+    //     ],
+    //   ),
+    // );
+     Get.dialog(Text("Welcome to flutter"));
+  
+  }
 
-//   }
+}
+
+
